b1d5fd787de409e99ca9367bbe3f81f838a9af41
ORLANDO BUSSOLO NETO <79544305+OrlandoBussolo@users.noreply.github.com>
2024-03-29 15:39:11 -0300

    Update Corr_Coef.cs

--- Corr_Coef.cs
+++ Corr_Coef.cs
@@ -1,135 +1,37 @@
 Corr_Coef = 
-VAR n = DISTINCTCOUNT(kpi_sumarized[Date])
+VAR CurrentX = SELECTEDVALUE(Kpi_list[Value])
+VAR CurrentY = SELECTEDVALUE(Kpi_list_2[Value])
+VAR X_Column = SELECTCOLUMNS(kpi_sumarized, "X", 
+                SWITCH(CurrentX,
+                    "BBAS3", [BBAS3],
+                    "BBDC4", [BBDC4],
+                    "BPAC11", [BPAC11],
+                    "INBR32", [INBR32],
+                    "ITUB3", [ITUB3],
+                    "ROXO34", [ROXO34],
+                    "SANB11", [SANB11]
+                )
+            )
+VAR Y_Column = SELECTCOLUMNS(kpi_sumarized, "Y", 
+                SWITCH(CurrentY,
+                    "BBAS3", [BBAS3],
+                    "BBDC4", [BBDC4],
+                    "BPAC11", [BPAC11],
+                    "INBR32", [INBR32],
+                    "ITUB3", [ITUB3],
+                    "ROXO34", [ROXO34],
+                    "SANB11", [SANB11]
+                )
+            )
+VAR Corr_Coef_XY = SUMX(FILTER(ADDXATABLE(X_Column,Y_Column),ISNUMBER([X])&&ISNUMBER([Y])), [X]*[Y])
+VAR Corr_Coef_X = SUMX(FILTER(ADDXATABLE(X_Column,Y_Column),ISNUMBER([X])&&ISNUMBER([Y])), [X])
+VAR Corr_Coef_Y = SUMX(FILTER(ADDXATABLE(X_Column,Y_Column),ISNUMBER([X])&&ISNUMBER([Y])), [Y])
+VAR Corr_Coef_X2 = SUMX(FILTER(ADDXATABLE(X_Column,Y_Column),ISNUMBER([X])&&ISNUMBER([Y])), [X]*[X])
+VAR Corr_Coef_Y2 = SUMX(FILTER(ADDXATABLE(X_Column,Y_Column),ISNUMBER([X])&&ISNUMBER([Y])), [Y]*[Y])
+
 RETURN
-DIVIDE((n*[Corr_coef_XY]-[Corr_coef_X]*[Corr_coef_Y]),
-        SQRT((n*[Corr_coef_X2]-[Corr_coef_X]^2)*(n*[Corr_coef_Y2]-[Corr_coef_Y]^2)),0)
-  
-Corr_coef_X = 
-VAR CurrentX = SELECTEDVALUE(Kpi_list[Value])
-VAR CurrentY = SELECTEDVALUE(Kpi_list_2[Value])
-VAR V_table  = SUMMARIZE(
-        kpi_sumarized,
-        kpi_sumarized[Date],
-        "x", SWITCH(TRUE(),  CurrentX = "BBAS3" ,  MAX(kpi_sumarized[BBAS3]),
-                             CurrentX = "BBDC4" ,  MAX(kpi_sumarized[BBDC4]),
-                             CurrentX = "BPAC11",  MAX(kpi_sumarized[BPAC11]),
-                             CurrentX = "INBR32",  MAX(kpi_sumarized[INBR32]),
-                             CurrentX = "ITUB3" ,  MAX(kpi_sumarized[ITUB3]),
-                             CurrentX = "ROXO34",  MAX(kpi_sumarized[ROXO34]),
-                             CurrentX = "SANB11",  MAX(kpi_sumarized[SANB11])),
-
-        "y", SWITCH(TRUE(),  CurrentY = "BBAS3" , MAX(kpi_sumarized[BBAS3]),
-                             CurrentY = "BBDC4" , MAX(kpi_sumarized[BBDC4]),
-                             CurrentY = "BPAC11", MAX(kpi_sumarized[BPAC11]),
-                             CurrentY = "INBR32", MAX(kpi_sumarized[INBR32]),
-                             CurrentY = "ITUB3" , MAX(kpi_sumarized[ITUB3]),
-                             CurrentY = "ROXO34", MAX(kpi_sumarized[ROXO34]),
-                             CurrentY = "SANB11", MAX(kpi_sumarized[SANB11]))
-                        )                       
-RETURN 
-SUMX(V_table, [x])
-
-Corr_coef_X2 = 
-VAR CurrentX = SELECTEDVALUE(Kpi_list[Value])
-VAR CurrentY = SELECTEDVALUE(Kpi_list_2[Value])
-VAR V_table  = SUMMARIZE(
-        kpi_sumarized,
-        kpi_sumarized[Date],
-        "x", SWITCH(TRUE(),  CurrentX = "BBAS3" ,  MAX(kpi_sumarized[BBAS3]),
-                             CurrentX = "BBDC4" ,  MAX(kpi_sumarized[BBDC4]),
-                             CurrentX = "BPAC11",  MAX(kpi_sumarized[BPAC11]),
-                             CurrentX = "INBR32",  MAX(kpi_sumarized[INBR32]),
-                             CurrentX = "ITUB3" ,  MAX(kpi_sumarized[ITUB3]),
-                             CurrentX = "ROXO34",  MAX(kpi_sumarized[ROXO34]),
-                             CurrentX = "SANB11",  MAX(kpi_sumarized[SANB11])),
-
-        "y", SWITCH(TRUE(),  CurrentY = "BBAS3" , MAX(kpi_sumarized[BBAS3]),
-                             CurrentY = "BBDC4" , MAX(kpi_sumarized[BBDC4]),
-                             CurrentY = "BPAC11", MAX(kpi_sumarized[BPAC11]),
-                             CurrentY = "INBR32", MAX(kpi_sumarized[INBR32]),
-                             CurrentY = "ITUB3" , MAX(kpi_sumarized[ITUB3]),
-                             CurrentY = "ROXO34", MAX(kpi_sumarized[ROXO34]),
-                             CurrentY = "SANB11", MAX(kpi_sumarized[SANB11]))
-                        )                       
-RETURN 
-SUMX(V_table, [x]*[x])
-
-Corr_coef_XY = 
-VAR CurrentX = SELECTEDVALUE(Kpi_list[Value])
-VAR CurrentY = SELECTEDVALUE(Kpi_list_2[Value])
-VAR V_table  = SUMMARIZE(
-        kpi_sumarized,
-        kpi_sumarized[Date],
-        "x", SWITCH(TRUE(),  CurrentX = "BBAS3" ,  MAX(kpi_sumarized[BBAS3]),
-                             CurrentX = "BBDC4" ,  MAX(kpi_sumarized[BBDC4]),
-                             CurrentX = "BPAC11",  MAX(kpi_sumarized[BPAC11]),
-                             CurrentX = "INBR32",  MAX(kpi_sumarized[INBR32]),
-                             CurrentX = "ITUB3" ,  MAX(kpi_sumarized[ITUB3]),
-                             CurrentX = "ROXO34",  MAX(kpi_sumarized[ROXO34]),
-                             CurrentX = "SANB11",  MAX(kpi_sumarized[SANB11])),
-
-        "y", SWITCH(TRUE(),  CurrentY = "BBAS3" , MAX(kpi_sumarized[BBAS3]),
-                             CurrentY = "BBDC4" , MAX(kpi_sumarized[BBDC4]),
-                             CurrentY = "BPAC11", MAX(kpi_sumarized[BPAC11]),
-                             CurrentY = "INBR32", MAX(kpi_sumarized[INBR32]),
-                             CurrentY = "ITUB3" , MAX(kpi_sumarized[ITUB3]),
-                             CurrentY = "ROXO34", MAX(kpi_sumarized[ROXO34]),
-                             CurrentY = "SANB11", MAX(kpi_sumarized[SANB11]))
-                        )                       
-RETURN 
-SUMX(V_table, [x]*[y])
-
-Corr_coef_Y = 
-VAR CurrentX = SELECTEDVALUE(Kpi_list[Value])
-VAR CurrentY = SELECTEDVALUE(Kpi_list_2[Value])
-VAR V_table  = SUMMARIZE(
-        kpi_sumarized,
-        kpi_sumarized[Date],
-        "x", SWITCH(TRUE(),  CurrentX = "BBAS3" ,  MAX(kpi_sumarized[BBAS3]),
-                             CurrentX = "BBDC4" ,  MAX(kpi_sumarized[BBDC4]),
-                             CurrentX = "BPAC11",  MAX(kpi_sumarized[BPAC11]),
-                             CurrentX = "INBR32",  MAX(kpi_sumarized[INBR32]),
-                             CurrentX = "ITUB3" ,  MAX(kpi_sumarized[ITUB3]),
-                             CurrentX = "ROXO34",  MAX(kpi_sumarized[ROXO34]),
-                             CurrentX = "SANB11",  MAX(kpi_sumarized[SANB11])),
-
-        "y", SWITCH(TRUE(),  CurrentY = "BBAS3" , MAX(kpi_sumarized[BBAS3]),
-                             CurrentY = "BBDC4" , MAX(kpi_sumarized[BBDC4]),
-                             CurrentY = "BPAC11", MAX(kpi_sumarized[BPAC11]),
-                             CurrentY = "INBR32", MAX(kpi_sumarized[INBR32]),
-                             CurrentY = "ITUB3" , MAX(kpi_sumarized[ITUB3]),
-                             CurrentY = "ROXO34", MAX(kpi_sumarized[ROXO34]),
-                             CurrentY = "SANB11", MAX(kpi_sumarized[SANB11]))
-                        )                       
-RETURN 
-SUMX(V_table, [y])
-
-Corr_coef_Y2 = 
-VAR CurrentX = SELECTEDVALUE(Kpi_list[Value])
-VAR CurrentY = SELECTEDVALUE(Kpi_list_2[Value])
-VAR V_table  = SUMMARIZE(
-        kpi_sumarized,
-        kpi_sumarized[Date],
-        "x", SWITCH(TRUE(),  CurrentX = "BBAS3" ,  MAX(kpi_sumarized[BBAS3]),
-                             CurrentX = "BBDC4" ,  MAX(kpi_sumarized[BBDC4]),
-                             CurrentX = "BPAC11",  MAX(kpi_sumarized[BPAC11]),
-                             CurrentX = "INBR32",  MAX(kpi_sumarized[INBR32]),
-                             CurrentX = "ITUB3" ,  MAX(kpi_sumarized[ITUB3]),
-                             CurrentX = "ROXO34",  MAX(kpi_sumarized[ROXO34]),
-                             CurrentX = "SANB11",  MAX(kpi_sumarized[SANB11])),
-
-        "y", SWITCH(TRUE(),  CurrentY = "BBAS3" , MAX(kpi_sumarized[BBAS3]),
-                             CurrentY = "BBDC4" , MAX(kpi_sumarized[BBDC4]),
-                             CurrentY = "BPAC11", MAX(kpi_sumarized[BPAC11]),
-                             CurrentY = "INBR32", MAX(kpi_sumarized[INBR32]),
-                             CurrentY = "ITUB3" , MAX(kpi_sumarized[ITUB3]),
-                             CurrentY = "ROXO34", MAX(kpi_sumarized[ROXO34]),
-                             CurrentY = "SANB11", MAX(kpi_sumarized[SANB11]))
-                        )                       
-RETURN 
-SUMX(V_table, [y]*[y])
-        
-        
-        
-        
-        
+DIVIDE(
+    COUNTROWS(X_Column) * Corr_Coef_XY - Corr_Coef_X * Corr_Coef_Y,
+    SQRT((COUNTROWS(X_Column) * Corr_Coef_X2 - Corr_Coef_X^2) * (COUNTROWS(X_Column) * Corr_Coef_Y2 - Corr_Coef_Y^2)),
+    0
+)
